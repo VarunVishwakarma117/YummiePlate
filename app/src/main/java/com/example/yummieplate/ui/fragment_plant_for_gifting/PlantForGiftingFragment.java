a688b16140041a1bc01f619294971c08943c8e6e
Abhishek <abhishekmohitcool31@gmail.com>
2021-05-06 18:37:50 +0530

    FlowerNBoquetFragment And PlantForGiftingFragment

--- app/src/main/java/com/example/yummieplate/ui/fragment_plant_for_gifting/PlantForGiftingFragment.java
+++ app/src/main/java/com/example/yummieplate/ui/fragment_plant_for_gifting/PlantForGiftingFragment.java
@@ -1,67 +1,98 @@
 package com.example.yummieplate.ui.fragment_plant_for_gifting;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.example.yummieplate.ListAdapter;
 import com.example.yummieplate.R;
+import com.example.yummieplate.item;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PlantForGiftingFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
 public class PlantForGiftingFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ProgressDialog progressDialog;
+    DatabaseReference all_itemRef = FirebaseDatabase.getInstance().getReference("admin")
+            .child("all_items").child("pizzas");
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    String cake_discription = "Freshly made on order\n" +
+            "Delivery within two hours\n" +
+            "Customisable quantity\n" +
+            "Best taste in city by Yummie plate";
 
-    public PlantForGiftingFragment() {
-        // Required empty public constructor
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_plant_for_gifting, container, false);
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlantForGiftingFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PlantForGiftingFragment newInstance(String param1, String param2) {
-        PlantForGiftingFragment fragment = new PlantForGiftingFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+        final ArrayList<item> items = new ArrayList<>();
+        progressDialog = new ProgressDialog(getActivity());
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_dialog_view);
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        ConnectivityManager manager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        if (networkInfo != null) {
+            all_itemRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+                        for (DataSnapshot dss : snapshot.getChildren()) {
+                            item i = dss.getValue(item.class);
+                            Log.e("TAG", "onDataChange: " + i);
+                            items.add(i);
+                            Log.v("range", i.getItem_PriceRange());
+                        }
+                        progressDialog.dismiss();
+                        ListAdapter itemsAdapter = new ListAdapter(getActivity(), items, false);
+                        ListView listView = root.findViewById(R.id.list);
+                        listView.setAdapter(itemsAdapter);
+                    } else {
+                        progressDialog.dismiss();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "Check Your Internet Connection", Toast.LENGTH_SHORT).show();
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View root =  inflater.inflate(R.layout.fragment_plant_for_gifting, container, false);
+
+//        items.add(new item(201, "Baked Kid’s pasta", cake_discription, null , "Chotu Packet-Badu Packet",
+//                null, null, R.drawable.p2001, "₹99.00 – ₹199.00", new HashMap<String,Integer>(){{
+//                    put("0000",99);
+//                    put("0100",199);}}));
+//
+//        items.add(new item(201, "Baked Macaroni pasta", cake_discription, null , "Half Plate-Full Plate",
+//                null, null, R.drawable.p2002, "₹99.00 – ₹199.00", new HashMap<String,Integer>(){{
+//            put("0000",199);
+////            put("0100",299);}}));
+//
+//        all_itemRef.setValue(items);           // can be use to store new value or to debug gImage overwrite bug
+
         return root;
     }
 }
